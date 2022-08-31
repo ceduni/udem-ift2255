cbc7435c35bf9e57b3108920c8e8836fd6dc7b0b
Louis-Edouard <lelafontant@yahoo.ca>
2022-08-31 00:11:21 -0400

    plan de cours automne 2022 pret

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
-import { getElement, Collapsible, getTemplate, isNullOrWhitespace, hasOwn, htmlToElement } from 'zenkai'
+import { getElement, getElements, Collapsible, getTemplate, hasOwn, htmlToElement } from 'zenkai'
 // import './assets/css/normalize.css';
 // import './assets/css/base.css';
 // import './assets/css/style.css';
 
-import data from './data/teachers.yml';
+import TEACHERS_DATA from './data/teachers.yml';
+import WEEK_DATA from './data/weeks.yml';
 
 
 let body = getElement('.body-content');
 
 let teachers = getElement(`[data-name="teachers"]`);
 
-if(teachers) {
+let schedule = getElements(`[data-name="schedule"]`);
+
+let weeks = getElement(`[data-name="weeks"]`);
+
+if (teachers) {
     let tplTeacher = getTemplate("#tpl-teacher");
     let content = tplTeacher.innerHTML;
-    
-    data.forEach(teacher => {
+
+    TEACHERS_DATA.forEach(teacher => {
         let tplElement = htmlToElement(parse(content, teacher));
         teachers.append(tplElement);
     });
 }
 
+if (weeks) {
+    let tplWeek = getTemplate("#tpl-week");
+    let content = tplWeek.innerHTML;
+
+    WEEK_DATA.forEach(data => {
+        let tplElement = htmlToElement(parse(content, data));
+        let items = getElements(`[data-item]`, tplElement);
+        
+        items.forEach(it => {
+            const { item, bind } = it.dataset;
+
+            let bindData = data[bind];
+            let tplWeek = getTemplate(`#${item}`);
+            let content = tplWeek.innerHTML;
+
+            bindData.forEach(bd => {
+                let tplWeekElement = htmlToElement(parse(content, bd))
+                it.append(tplWeekElement);
+            })
+
+        })
+        weeks.append(tplElement);
+    });
+}
+
 Collapsible(body);
+
 
 function parse(content, data) {
     var result = content.trim();
